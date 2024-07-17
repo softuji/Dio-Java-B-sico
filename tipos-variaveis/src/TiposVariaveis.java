4b25adcb535854b654b29a51dba41a7cc11a532b
softuji <sofiatuji@gmail.com>
2024-07-17 20:40:30 -0300

    Update commit

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        String palavra = "aula";
+        System.out.println(palavra);
+    
+        int dobro = 2;
+        System.out.println(dobro);
+
+        double salario = 2300.15;
+        System.out.println(salario);
+
+        boolean alternativa = true;
+        System.out.println(alternativa);
+        
+        char abc = 'a';
+        System.out.println(abc);
+        
     }
 }
